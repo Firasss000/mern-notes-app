35956a8ace191f60cf6b5063e9f1a6f2479489b8
Firas Bendalla <123781423+Firasss000@users.noreply.github.com>
2026-01-17 15:11:14 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,8 +15,3 @@ app.use('/api/notes', notesRoutes);
 app.listen(PORT, () => {
   console.log('Server is running on localhost:', PORT);
 });
-
-
-// connection string for mongodb :
-// mongodb+srv://firasbnd_db_user:b9h5MLvNobdqhg3S@cluster0.jftwvhx.mongodb.net/?appName=Cluster0
-// pwd: b9h5MLvNobdqhg3S
