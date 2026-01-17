372e94d4904a8c78b1e951e5e93cf62f105b4cab
Firasss000 <Firas.bendalla@rades.r-iset.tn>
2026-01-17 13:13:51 +0100

    DB connection + Model creation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(5001, () => {
-  console.log('Server is running on localhost:5001');
+app.listen(PORT, () => {
+  console.log('Server is running on localhost:', PORT);
 });
+
+
+// connection string for mongodb :
+// mongodb+srv://firasbnd_db_user:b9h5MLvNobdqhg3S@cluster0.jftwvhx.mongodb.net/?appName=Cluster0
+// pwd: b9h5MLvNobdqhg3S
